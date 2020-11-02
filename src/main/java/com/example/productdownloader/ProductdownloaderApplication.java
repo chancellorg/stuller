bc2cf7a5632276d27befd50724ec367b86e13fb0
Chancellor.Galiza <cgaliza@osius.com>
2020-11-02 22:26:01 +0800

    current progress

--- src/main/java/com/example/productdownloader/ProductdownloaderApplication.java
+++ src/main/java/com/example/productdownloader/ProductdownloaderApplication.java
@@ -8,13 +8,17 @@ import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
+import java.util.Scanner;
 
 
 @SpringBootApplication
 public class ProductdownloaderApplication {
 
     private static String url = "https://api.stuller.com/v2/products/";
+
+    private static String filename = "products\\dump";
 
     public static void main(String[] args) throws Exception{
         SpringApplication.run(ProductdownloaderApplication.class, args);
@@ -25,17 +29,28 @@ public class ProductdownloaderApplication {
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
         RestTemplate restTemplate = restTemplateBuilder.basicAuthentication("jandjdev","Welcome1").build();
 
-        String response = restTemplate.getForObject(url,String.class);
+        String response = "";
+        String nextPage = "";
 
-        String filename = "products\\dump";
-        String nextPage = getNextPage(response);
-        int ctr = 1;
+        Scanner scanner = new Scanner(new File("productTracker.txt"));
+        int ctr = scanner.nextInt();
+        nextPage = scanner.next();
+        scanner.close();
+
+        if(ctr == 0) {
+            response = restTemplate.getForObject(url, String.class);
+            nextPage = getNextPage(response);
+            ctr = 1;
+        } else {
+
+
+        }
+
         while(nextPage!= null) {
 
             // write to file
             BufferedWriter writer = new BufferedWriter(new FileWriter(filename + ctr + ".json"));
             writer.write(response);
-
             writer.close();
 
             System.out.println(ctr++ + " == "+nextPage);
@@ -44,13 +59,9 @@ public class ProductdownloaderApplication {
 
             HttpEntity<String> entity = new HttpEntity<String>(request.toString(), headers);
             response = restTemplate.postForEntity(url, entity, String.class).getBody();
-
-
-
             nextPage = getNextPage(response);
         }
     }
-
 
     private static String getNextPage(String response) throws Exception{
         return new JSONObject(response).getString("NextPage");
